81f87a8bf3bdea2e6a1bd4d04652fa46eb9c22e6
Franklin Velasquez <fran.velz25@gmail.com>
2026-02-10 23:29:05 -0600

    Completando Seccion 2: Pruebas de API PetStore

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,8 +1,8 @@
 describe('Prueba API - PetStore', () => {
-  it('Validar creación y consulta de mascota', () => {
+  it('Flujo completo: Crear, Consultar, Actualizar y Validar por Estado', () => {
     const id = 556677;
     
-    // Crear Mascota
+    // 1. Añadir Mascota (POST)
     cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
       id: id,
       name: "CypressPet",
@@ -10,11 +10,27 @@ describe('Prueba API - PetStore', () => {
     }).then((res) => {
       expect(res.status).to.eq(200);
       
-      // Consultar Mascota
-      cy.request('GET', `https://petstore.swagger.io/v2/pet/${id}`)
-        .then((resGet) => {
-          expect(resGet.body.name).to.eq("CypressPet");
+      // 2. Consultar por ID (GET)
+      cy.request('GET', `https://petstore.swagger.io/v2/pet/${id}`).then((resGet) => {
+        expect(resGet.body.name).to.eq("CypressPet");
+
+        // 3. Actualizar Nombre y Estado a "sold" (PUT) 
+        cy.request('PUT', 'https://petstore.swagger.io/v2/pet', {
+          id: id,
+          name: "CypressPet-Actualizado",
+          status: "sold"
+        }).then((resPut) => {
+          expect(resPut.status).to.eq(200);
+
+          // 4. CONSULTAR POR ESTADO: Buscar las vendidas (GET) 
+          cy.request('GET', 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold').then((resStatus) => {
+            expect(resStatus.status).to.eq(200);
+            // Validar que nuestra mascota está en la lista de vendidos
+            const mascota = resStatus.body.find(p => p.id === id);
+            expect(mascota.status).to.eq("sold");
+          });
         });
+      });
     });
   });
 });
